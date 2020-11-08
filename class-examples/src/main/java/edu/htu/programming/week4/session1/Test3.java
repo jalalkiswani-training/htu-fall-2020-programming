3afd9c0e30d761f5aa89cd65a35e3e293760adb1
Jalal Kiswani <kiswanij@gmail.com>
2020-11-08 12:47:29 +0200

    .

---
+++ class-examples/src/main/java/edu/htu/programming/week4/session1/Test3.java
@@ -0,0 +1,9 @@
+package edu.htu.programming.week4.session1;
+
+import javax.swing.JOptionPane;
+
+public class Test3 {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"Hello");
+	}
+}
